dc24ecc9e819ec104ad933bda7966d2d5cade29a
KanitaNiki <kanitaafrin@gmail.com>
2022-12-15 12:32:45 -0500

    updated app.js and server.js

--- server.js
+++ server.js
@@ -1,19 +1,65 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+const projectData = {};
 
 // Require Express to run server and routes
+const express = require ('express');
 
 // Start up an instance of app
 
+const app = express();
+
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require ('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+const port = 3000;
+
+const server = app.listen(port, listening);
+
+function listening(){
+    console.log('server is running');
+    console.log(`server is running on ${port}`);
+}
+
+// Add a GET route that returns the projectData object in your server code 
+
+app.get('/mostRecent',mostRecent);
+
+function mostRecent( request, response){
+    response.send(projectData);
+}
+
+// Then, add a POST route that adds incoming data to projectData.
+// incoming data: temperature, date, response
+
+const data =[];
+
+app.post('/addMostRecentData', addMostRecentData)
+
+function addMostRecentData (req,res){
+
+    newEntry = {
+        temp: req.body.temp,
+        content: req.body.content,
+        date:req.body.date
+    
+    }
+
+    data.push(newEntry)
+    console.log(data)
+}
+
+
+
+
--- website/app.js
+++ website/app.js
@@ -1,5 +1,90 @@
+// Personal API Key for OpenWeatherMap API
 /* Global Variables */
+
+const baseURL= 'http://api.openweathermap.org/data/2.5/forecast?zip=&appid=eda647e59ce510ca324ce6591fa4e948';
+const apiKey = 'eda647e59ce510ca324ce6591fa4e948&units=imperial';
+
+const generateButton = document.getElementById('generate').addEventListener('click', performAction);
+
+function performAction(e){
+    const zipCode = document.getElementById('zip').value ;
+    getWeatherByZipCode(baseURL,zipCode,apiKey)
+
+}
+
+//requesting for weather info from openweather map
+const getWeatherByZipCode = async(baseURL,zipCode,apiKey)=>{
+
+
+    const res = await fetch(baseURL+zipCode+key)
+
+    try{
+    
+        const data = await res.json();
+        console.log(data)
+
+    }   catch(error){
+        console.log("error",error);
+    }
+
+    return 
+}
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+console.log(d);
+
+
+// Client side POST route:
+
+const postData = async ( url = '', data = {})=>{
+    console.log(data);
+      const response = await fetch(url, {
+      method: 'POST', 
+      credentials: 'same-origin',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+     // Body data type must match "Content-Type" header        
+      body: JSON.stringify(data), 
+    });
+
+      try {
+        const newData = await response.json();
+        console.log(newData);
+        return newData;
+      }catch(error) {
+      console.log("error", error);
+      }
+  }
+
+postData('/addMostRecentData',{})
+
+// Client Side GET route: There should be an asynchronous function 
+// to fetch the data from the app endpoint.
+// Function to GET Project Data:
+
+const retrieveData = async () =>{
+ const request = await fetch('/all');
+ try {
+ // Transform into JSON
+ const allData = await request.json()
+ console.log(allData)
+ // Write updated data to DOM elements
+ document.getElementById('temp').innerHTML = Math.round(allData.temp)+ 'degrees';
+ document.getElementById('content').innerHTML = allData.feel;
+ document.getElementById("date").innerHTML =allData.date;
+ }
+ catch(error) {
+   console.log("error", error);
+//    appropriately handle the error
+ }
+}
+
+
+
+
+
+
+
